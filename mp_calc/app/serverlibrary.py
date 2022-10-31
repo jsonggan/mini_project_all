3b7ac4f0930b8359756b73145abc1290e8dc4f11
ibra2407 <113652688+ibra2407@users.noreply.github.com>
2022-10-31 14:33:17 +0800

    da

--- mp_calc/app/serverlibrary.py
+++ mp_calc/app/serverlibrary.py
@@ -1,7 +1,50 @@
 
 
+def merge(array, p, q, r, byfunc=None):
+    nleft = q - p +1
+    nright = r - q
+    left_array = array[p:q+1]
+    right_array = array[q+1:r+1]
+    left = 0
+    right = 0
+    dest = p
+    
+    while left<nleft and right<nright:
+        compare_leq = None
+        if byfunc == None:
+            compare_leq = left_array[left]<=right_array[right]
+        else:
+            compare_leq = byfunc(left_array[left])<=byfunc(right_array[right])
+
+        if compare_leq:
+            array[dest] = left_array[left]
+            left += 1
+            dest += 1
+        
+        else:
+            array[dest] = right_array[right]
+            right += 1
+            dest += 1
+    
+    while left<nleft:
+        array[dest] = left_array[left]
+        left += 1
+        dest += 1
+    while right<nright:
+        array[dest] = right_array[right]
+        right += 1
+        dest += 1
+
+def mergesort_recursive(array, p, r, byfunc=None):
+    if r -p >0:
+        q = int((p+r)/2)
+        mergesort_recursive(array,p,q,byfunc)
+        mergesort_recursive(array,q+1,r,byfunc)
+        merge(array,p,q,r,byfunc)
+        
+
 def mergesort(array, byfunc=None):
-  pass
+    mergesort_recursive(array,0,len(array)-1,byfunc)
 
 class Stack:
   pass
